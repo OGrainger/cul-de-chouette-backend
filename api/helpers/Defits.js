ba567fbe6733236e27132bdd1116cfa6c42a474f
Clément <clem-lambert@hotmail.fr>
2018-12-13 14:59:48 +0100

    [UPD]combinaisons

---
+++ api/helpers/Defits.js
@@ -0,0 +1,25 @@
+/**
+ * @return {boolean}
+ */
+function Sirop(C1, C2, CAgain) {
+    if (Chouette){
+        CAgain=ChouetteGenerator();
+        if (CuldeChouette(C1, C2, CAgain)){
+            return true;
+        }
+    }
+    return error
+}
+//reste a faire si possible
+
+// function Civet (){
+//
+// }
+
+// function Grelottine() {
+//
+// }
+
+// function Bevue() {
+//
+// }
